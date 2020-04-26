df9f289d756c71269bcc621ae98fdef7fa327f0c
yolberjp <yolberjp@gmail.com>
2020-04-26 19:57:17 +0200

    Fix config.js

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Database
 let urlDB;
 
-if(process.env.NODE_ENV != 'dev'){
+if(process.env.NODE_ENV == 'dev'){
     urlDB = 'mongodb://localhost:27017/coffee';
 }else{
     urlDB = 'mongodb+srv://yolberjp:d4d8JfEVx1W62qbn@cluster0-s5njb.mongodb.net/coffee';
