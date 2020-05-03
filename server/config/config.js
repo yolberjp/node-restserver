d6fa336033af70732ec3adb990f759151f0919b0
yolberjp <yolberjp@gmail.com>
2020-05-03 13:06:32 +0200

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if(process.env.NODE_ENV == 'dev'){
     urlDB = 'mongodb://localhost:27017/coffee';
 }else{
-    urlDB = 'mongodb+srv://yolberjp:d4d8JfEVx1W62qbn@cluster0-s5njb.mongodb.net/coffee';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
